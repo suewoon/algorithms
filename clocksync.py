632bc572f8d2c426be018fece92a1e77d1fbbb25
suewoon <suewoon.ryu@gmail.com>
2017-02-03 07:24:16 +0900

    fix sintax error , add main function

--- clocksync.py
+++ clocksync.py
@@ -1,66 +1,64 @@
 # !/usr/bin/env
 # clocksync.py
 
-from sys import stdin
-import numpy as np
+import sys
 
-INF = 999, SWITCHES = 10, CLOCKS = 16
+INF = 999
+SWITCHES = 10
+CLOCKS = 16
 
-linked = [[ 3,  3,  3.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-         0.,  0.,  0.],
-       [ 0.,  0.,  0.,  3,  0.,  0.,  0.,  3,  0.,  3,  0.,  3,  0.,
-         0.,  0.,  0.],
-       [ 0.,  0.,  0.,  0.,  3,  0.,  0.,  0.,  0.,  0.,  3,  0.,  0.,
-         0.,  3,  3],
-       [ 3,  0.,  0.,  3,  3,  3,  3,  0.,  0.,  0.,  0.,  0.,  0.,
-         0.,  0.,  0.],
-       [ 0.,  0.,  0.,  0.,  0.,  0.,  3,  3,  3,  0., 3,  0.,  3,
-         0.,  0.,  0.],
-       [ 3,  0.,  3,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-         0.,  3,  3],
-       [ 0.,  0.,  0.,  3,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-         0.,  3,  3],
-        [ 0.,  0.,  0.,  0.,  3,  3,  0.,  3,  0.,  0.,  0.,  0.,  0.,
-         0.,  3,  3],
-       [ 0.,  3,  3,  3,  3,  3,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-         0.,  0.,  0.],
-       [ 0.,  0.,  0.,  3,  3,  3,  0.,  0.,  0.,  3,  0.,  0.,  0.,
-         3,  0.,  0.]]
+linked = [[3, 3, 3., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+           0., 0., 0.],
+          [0., 0., 0., 3, 0., 0., 0., 3, 0., 3, 0., 3, 0.,
+           0., 0., 0.],
+          [0., 0., 0., 0., 3, 0., 0., 0., 0., 0., 3, 0., 0.,
+           0., 3, 3],
+          [3, 0., 0., 3, 3, 3, 3, 0., 0., 0., 0., 0., 0.,
+           0., 0., 0.],
+          [0., 0., 0., 0., 0., 0., 3, 3, 3, 0., 3, 0., 3,
+           0., 0., 0.],
+          [3, 0., 3, 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+           0., 3, 3],
+          [0., 0., 0., 3, 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+           0., 3, 3],
+          [0., 0., 0., 0., 3, 3, 0., 3, 0., 0., 0., 0., 0.,
+           0., 3, 3],
+          [0., 3, 3, 3, 3, 3, 0., 0., 0., 0., 0., 0., 0.,
+           0., 0., 0.],
+          [0., 0., 0., 3, 3, 3, 0., 0., 0., 3, 0., 0., 0.,
+           3, 0., 0.]]
 
-clockboard = np.zeros(16)
-print(clockboard)
+
+def main():
+    testcases = sys.stdin.readline()
+    print('Try %s times..', str(testcases))
+    for line in sys.stdin():
+        print(findMininum(list(line), 0))
+
+
+if __name__ == "__main__": main()
+
 
 def areAligned(clockboard):
-    for i in range(0,16):
-        if clockboard[i]!=12:
+    for i in range(0, 16):
+        if clockboard[i] != 12:
             return False
     return True
 
+
 def press(clockboard, switch):
-        clockboard+=linked[switch]
-        for i in range(0,16):
-            if clockboard[i] == 15:
-                clockboard[i] -= 3
+    clockboard += linked[switch]
+    for i in range(0, 16):
+        if clockboard[i] == 15:
+            clockboard[i] -= 3
 
-#switch는 다음에 누를 스위치 번호
+
 def findMininum(clockboard, switch):
-    pressedSwitch=0
+    if (switch == SWITCHES):
+        return 0 if areAligned(clockboard) else INF
+    minimumTimes = INF
+    for i in range(0, 4):
+        minimumTimes = min(minimumTimes, i + findMininum(clockboard, switch + 1))
+        press(clockboard, switch)
 
-    if areAligned():
-        return pressedSwitch
-
-    for i in range(0,4):
-
-        press(clockboard,switch)
-        pressedSwitch = findMininum(clockboard,switch+1)
-
-for trial in range(0,numOfTrials):
-    clockboard = [[int(n) for n in line.split('')] for line in stdin.readline()]
-    print(clockBoard)
-    solve(cl)
-
-
-
-numOfTrials = int(stdin.readline())
-print('# of trials : %s'%numOfTrials)
-
+    return minimumTimes
