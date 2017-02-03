67d25682a7ec79fae79863902e97d10e8b6d9d34
suewoon <suewoon.ryu@gmail.com>
2017-02-04 01:09:56 +0900

    add main method

--- boggleGame.py
+++ boggleGame.py
@@ -1,36 +1,30 @@
 # !/usr/bin/env
 # boggleGame.py
 
-import os
-import numpy as np
-
-"""board= [['N','N','N','N','S'],['N','E','E','E','N'],
-         ['N','E','Y','E','N'],['N','E','E','E','N'],
-         ['N','N','N','N','N']]"""
-
-inputFile = open(os.path.join('input1.txt', os.getcwd()),'r')
-data = [[str(n)] for n in line.split()] for line in inputFile
-lines = inputFile.readlines()
-board = np.array(' ',5*5)
-numOfTrials = lines[0]
-lines = lines[1:]
-for i in range(0, numOfTrials):
-    for i in (0,24):
-        board[i] = lines[]
-    testWords = lines[5]
-    lines=lines[6+testWords:]
+import sys
 
 dx = [-1,-1,-1,1,1,1,0,0]
 dy = [-1,0,1,-1,0,1,-1,1]
 
+def main():
+    testcases = int(sys.stdin.readline())
+    for i in range(0, testcases):
+        board=[]
+        for j in range(0,5):
+            board.append(list(sys.stdin.readline())[:-1])
+        givenWords = int(sys.stdin.readline())
+        for k in range(0,givenWords):
+            word = sys.stdin.readline()
+            print(word)
+
+
+#todo :  adjecent 8 characters from starting point (x,y)
 def hasWord(x,y, word):
 
     if x not in range(0, len(word)) or y not in range(0,len(word)):
-        print('Out of Range')
         return
 
     if board[x][y] != word[0]:
-        print('Could not find')
         return
 
     if(len(word)==1):
@@ -43,4 +37,5 @@ def hasWord(x,y, word):
 
     return False
 
-hasWord(2,2,'YES')
+
+if __name__ = "__main__" : main()
