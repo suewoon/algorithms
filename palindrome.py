05f0db36685e3e40cae5e7c43d417e29284fdadf
suewoon <suewoon.ryu@gmail.com>
2017-06-03 00:43:42 +0900

    create a new file

--- palindrome.py
+++ palindrome.py
@@ -8,19 +8,23 @@ Created on Sun May 14 10:59:25 2017
 https://www.acmicpc.net/problem/1695
 12135 
 """
+
 import sys
 sys.setrecursionlimit(10000)
+#---> 왼쪽 오른쪽 번갈아서 떼보고 최소값을 리턴 
+#def make_palindrome(seq,count):
+#    if len(seq) < 2  : return count 
+#    if seq[0] != seq[-1] : 
+#        count+=1
+#        seq.append(seq[0])
+#    return make_palindrome(seq[1:-1],count)
 
 def make_palindrome(seq,count):
-    if len(seq) < 2  : return count 
-    if seq[0] != seq[-1] : 
-        count+=1
-        seq.append(seq[0])
-    return make_palindrome(seq[1:-1],count)
-
-def find_sub_palindrome(seq):
-    
-    return seq
+    if len(seq)< 2 : return count 
+    if seq[0]==seq[-1]:
+        return make_palindrome(seq[1:-1],count)
+    else :
+        return min(make_palindrome(seq[1:],count),make_palindrome(seq[:-1],count))+1
 
 if __name__=='__main__':
     n = int(input())
