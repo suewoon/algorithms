861b404f23b0c0dc215eb0c714a44cae035791ac
suewoon <suewoon.ryu@gmail.com>
2017-07-28 19:22:45 +0900

    recursion

--- DP/picnic.py
+++ DP/picnic.py
@@ -1,54 +1,54 @@
 #!/usr/bin/env python3 
-#solution for : https://algospot.com/judge/problem/read/PICNIC
+# solution for : https://algospot.com/judge/problem/read/PICNIC
 
+class MakingPairs(object):
+
+    def __init__(self,n,l):
+        self.n_of_students = n
+        self.friends_pairs = l
+        self.are_friends = [[False]*n for i in range(n)]
+        self.check_friends()
+
+    def check_friends(self):
+        for i in range(0, len(self.friends_pairs), 2):
+            id1 = self.friends_pairs[i]
+            id2 = self.friends_pairs[i+1]
+            if id1>id2 :
+                self.are_friends[id2][id1]=True
+            else :
+                self.are_friends[id1][id2]=True
+
+    def count_pairs(self):
+        isTaken = [False]*self.n_of_students
+        set_of_pairs = self.count_pairs_helper(isTaken)
+        return set_of_pairs
+
+    def count_pairs_helper(self,isTaken):
+        available = -1
+        for i in range(self.n_of_students):
+            if not isTaken[i]:
+                available = i
+                break
+
+        if available == -1:
+            return 1
+
+        ans = 0
+
+        for partner in range(available+1, self.n_of_students):
+            if not isTaken[partner] and not isTaken[available] and self.are_friends[available][partner]:
+                isTaken[available] = isTaken[partner] = True
+                ans += self.count_pairs_helper(isTaken)
+                isTaken[available] = isTaken[partner] = False
+        return ans
 
 def main():
     test_cases  = int(input())
-    for i in range(0, testcases*2):
-        1st_line = list(int(i) for i in input())
-        2nd_line  = list(int(i) for i in input())
-        mp = MakingPairs(1st_line[0],1st_line[1],2nd_line)
-        print(mp.count_friend_pairs())
+    for c in range(0, test_cases):
+        (n,m)  = tuple(int(i) for i in input().split())
+        friends_pairs = list(int(i) for i in input().split())
+        mp = MakingPairs(n, friends_pairs)
+        print(mp.count_pairs())
 
-class MakingPairs(object):
-    def __init__(self,n,m,l):
-        self.n_of_students = n
-        self.n_of_friends_pairs = m
-        self.friends_pairs = l
-        self.are_friends = [[False]*n for i in range(n)]
-
-    def check_they_are_friends(self):
-        for i in range(0,len(self.are_friends),2):
-            id1 = self.friends_pairs[i]
-            id2 = self.friends_pairs[i+1] 
-            if id1>id2 : 
-                self.are_friends[id2][id1]=True 
-            else :
-                self.are_friends[id1][id2]=True 
-
-    #todo : count  a number of pairs if they're friends
-    def count_friend_pairs(self):
-        # return unmatched one yet
-        unmatched = -1
-        for i in range(0,numOfStudents):
-            if isTaken[i] is False :
-                unmatched = i
-                 break
-
-        #when everyone is matched
-            if(unmatched == -1):
-            return 1
-
-        #num of possible pairs
-        numOfPairs = 0
-
-         for i in range(unmatched+1, numOfStudents):
-            if isTaken[i] is False and areFriends[unmatched][i]:
-                isTaken[unmatched] = isTaken[i] = True
-                numOfPairs += countingPairs(isTaken)
-                isTaken[unmatched] = isTaken[i] = False
-
-        return numOf Pairs
-
-
-if __name__ == "__main__" : main()
+if __name__ == "__main__":
+    main()
--- DP/test_picnic.py
+++ DP/test_picnic.py
@@ -1,2 +1,16 @@
 #!usr/bin/env python3 
 #pytest for picnic.py
+
+from picnic import MakingPairs
+
+def test_count_pairs_1():
+    mp = MakingPairs(2,1,[0,1])
+    assert(mp.count_friend_pairs() == 1)
+
+def test_count_pairs_2():
+    mp = MakingPairs(4,6,[0,1,1,2,2,3,3,0,0,2, 1,3])
+    assert(mp.count_friend_pairs() == 3)
+
+
+
+
