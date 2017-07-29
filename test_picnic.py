5293ffe59bd7171323636175e094601666fdde7b
suewoon <suewoon.ryu@gmail.com>
2017-07-29 16:14:34 +0900

    test_count_pairs3()

--- test_picnic.py
+++ test_picnic.py
@@ -4,12 +4,16 @@
 from picnic import MakingPairs
 
 def test_count_pairs_1():
-    mp = MakingPairs(2,1,[0,1])
-    assert(mp.count_friend_pairs() == 1)
+    mp = MakingPairs(2,[0,1])
+    assert(mp.count_pairs() == 1)
 
 def test_count_pairs_2():
-    mp = MakingPairs(4,6,[0,1,1,2,2,3,3,0,0,2, 1,3])
-    assert(mp.count_friend_pairs() == 3)
+    mp = MakingPairs(4,[0,1,1,2,2,3,3,0,0,2, 1,3])
+    assert(mp.count_pairs() == 3)
+
+def test_count_pairs_3():
+    mp = MakingPairs(6, [0, 1, 0, 2, 1, 2, 1, 3, 1, 4, 2, 3, 2, 4, 3,  4,3,5,4, 5])
+    assert(mp.count_pairs() == 4)
 
 
 
