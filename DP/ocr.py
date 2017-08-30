7cdc45f86818be3515c683a1fac5955ae3c41703
suewoon <suewoon.ryu@gmail.com>
2017-08-30 17:04:04 +0900

    read_words()

--- DP/ocr.py
+++ DP/ocr.py
@@ -3,46 +3,60 @@ import numpy as np
 
 class Reader(object):
     def __init__(self, words):
-        self.words = words
-        self.first_pos_probs = []
-        self.cond_probs = []
-        self.classified_probs = []
+        self.words = words # of states
+        self.initial_probs = []
+        self.transition_probs = []
+        self.emission_probs = []
 
     def read_input(self, _input, param):
         _list = [float(x) for x in _input.split(' ')]
-        if param == 'first_pos_probs':
-            self.first_pos_probs.append(_list)
-        elif param == 'cond_probs':
-            self.cond_probs.append(_list)
-        elif param == 'classified_probs':
-            self.classified_probs.append(_list)
+        if param == 'initial_probs':
+            self.initial_probs.append(_list)
+        elif param == 'transition_probs':
+            self.transition_probs.append(_list)
+        elif param == 'emission_probs':
+            self.emission_probs.append(_list)
 
     def read_words(self, _input):
         _input = _input.split(' ')[1:]
-        print('input', _input)
-        ans = []
-        for word in _input:
-            if _input.index(word) == 0:
-                p = [(p_ab*p_a) for p_a, p_ab in zip(self.first_pos_probs[0], self.classified_probs[0]) if p_a != 0]
-            else:
-                idx = self.words.index(word)
-                pre_idx = self.words.index(ans[-1])
-                p = [(p_ab*p_a) for p_a, p_ab in zip(self.cond_probs[pre_idx],
-                                                     np.array(self.classified_probs)[:, idx]) if p_a != 0]
-            print(word, p, p.index(max(p)))
-            ans.append(self.words[p.index(max(p))])
-        return ' '.join(ans)
+        self.emission_probs = np.array(self.emission_probs)
+        self.transition_probs = np.array(self.transition_probs)
+        self.initial_probs = np.array(self.initial_probs)
+        
+        
+        T = len(_input) # observations
+        N = len(self.words) # sates
 
+        alpha = np.array([ [float('-inf') for _ in range(N)] for _ in range(T) ])#T(obs)*N(state) cache
+        backpointers = np.array([ [-1 for _ in range(N)] for _ in range(T)])
+
+        alpha[0, :] = self.initial_probs[0]*self.emission_probs[:,0]
+
+        for t in range(1, T):
+            for s1 in range(N):
+                for s2 in range(N):
+                    prob = alpha[t-1, s1]*self.transition_probs[s1, s2]*self.emission_probs[s2,t]
+                    if prob > alpha[t, s2]:
+                        alpha[t, s2] = prob
+                        backpointers[t, s2] = s1
+        words_seq = []
+        words_seq.append(np.argmax(alpha[T-1, :]))
+        for i in range(T-1, 0, -1):
+            words_seq.append(backpointers[i, words_seq[-1]])
+
+        ans = [self.words[i] for i in list(reversed(words_seq))]
+        return ' '.join(ans)
+    
 
 def main():
     m, q = tuple(int(x) for x in input().split(' '))
     words = input().split(' ')
     reader = Reader(words)
-    reader.read_input(input(), 'first_pos_probs')
+    reader.read_input(input(), 'initial_probs')
     for i in range(m):
-        reader.read_input(input(), 'cond_probs')
+        reader.read_input(input(), 'transition_probs')
     for i in range(m):
-        reader.read_input(input(), 'classified_probs')
+        reader.read_input(input(), 'emission_probs')
     for i in range(q):
         reader.read_words(input())
 
--- DP/test_ocr.py
+++ DP/test_ocr.py
@@ -6,27 +6,26 @@ from ocr import Reader
 class Test_Class(object):
     def test_read_input(self):
         reader = Reader(['I','am','a','boy','buy'])
-        reader.read_input('1.0 0.0 0.0 0.0 0.0', 'first_pos_probs')
+        reader.read_input('1.0 0.0 0.0 0.0 0.0', 'initial_probs')
         for _list in '0.1 0.6 0.1 0.1 0.1\n0.1 0.1 0.6 0.1 0.1\n0.1 0.1 0.1 0.6 0.1\n0.2 0.2 0.2 0.2 0.2\n0.2 0.2 0.2 0.2 0.2'.split('\n'):
-            reader.read_input(_list, 'cond_probs')
+            reader.read_input(_list, 'transition_probs')
         for _list in '0.8 0.1 0.0 0.1 0.0\n0.1 0.7 0.0 0.2 0.0\n0.0 0.1 0.8 0.0 0.1\n0.0 0.0 0.0 0.5 0.5\n0.0 0.0 0.0 0.5 0.5'.split('\n'):
-            reader.read_input(_list, 'classified_probs')
-        print(reader.first_pos_probs)
-        print(reader.cond_probs)
-        print(reader.classified_probs)
+            reader.read_input(_list, 'emission_probs')
+        print(reader.initial_probs)
+        print(reader.transition_probs)
+        print(reader.emission_probs)
         print(reader.words)
-    
         assert 0
 
     def test_read_words(self):
         reader = Reader(['I','am','a','boy','buy'])
-        reader.read_input('1.0 0.0 0.0 0.0 0.0', 'first_pos_probs')
+        reader.read_input('1.0 0.0 0.0 0.0 0.0', 'initial_probs')
         for _list in '0.1 0.6 0.1 0.1 0.1\n0.1 0.1 0.6 0.1 0.1\n0.1 0.1 0.1 0.6 0.1\n0.2 0.2 0.2 0.2 0.2\n0.2 0.2 0.2 0.2 0.2'.split('\n'):
-            reader.read_input(_list, 'cond_probs')
+            reader.read_input(_list, 'transition_probs')
         for _list in '0.8 0.1 0.0 0.1 0.0\n0.1 0.7 0.0 0.2 0.0\n0.0 0.1 0.8 0.0 0.1\n0.0 0.0 0.0 0.5 0.5\n0.0 0.0 0.0 0.5 0.5'.split('\n'):
-            reader.read_input(_list, 'classified_probs')
+            reader.read_input(_list, 'emission_probs')
        
         assert reader.read_words('4 I am a buy') == 'I am a boy'
-        #assert reader.read_words('4 I I a boy') == 'I am a boy'
+        assert reader.read_words('4 I I a boy') == 'I am a boy'
         assert reader.read_words('4 I am am boy') == 'I am a boy'
 
