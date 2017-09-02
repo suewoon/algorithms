5347a242694ddc7d2a72742dbcf67a022817e747
suewoon <suewoon.ryu@gmail.com>
2017-09-03 00:05:47 +0900

    measure()

--- DP/baekjoon_2839.py
+++ DP/baekjoon_2839.py
@@ -6,24 +6,30 @@ class Packing(object):
         self.weights = [3, 5]
 
     def measure(self, w_to_measure):
-        cache = {}
+        memo = {}
         def measure_helper(n, remaining):
-            if remaining in cache:
-                return cache[remaining]
-
-            if remaining <= 0:
+            if remaining in memo:
+                return memo[remaining]
+            elif remaining == 0:
                 return n
+            elif remaining < 2:
+                return float('inf')
+            else:
+                ans = min(measure_helper(n+1, remaining-self.weights[0]), measure_helper(n+1, remaining-self.weights[1])) 
+                if((remaining not in memo) or memo[remaining]>ans):
+                    memo[remaining] = ans
+                    print(memo)
+                return ans
 
-            ans = min(measure_helper(n+1, remaining-self.weights[0]),
-                       measure_helper(n+1, remaining-self.weights[1]))
-            cache[remaining] = ans
-            return ans
-        return measure_helper(0, w_to_measure)
-
+        measure_helper(0, w_to_measure)
+        if memo[w_to_measure] == float('inf'):
+            return -1
+        else:
+            return memo[w_to_measure]
 
 def main():
     packing = Packing()
-    print(packing.measure(int()))
+    print(packing.measure(int(input())))
 
 if __name__ == '__main__':
     main()
