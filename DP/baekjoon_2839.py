4638291c566f828785884290de050a59942c2e05
suewoon <suewoon.ryu@gmail.com>
2017-09-03 00:38:15 +0900

    measure() memo

--- DP/baekjoon_2839.py
+++ DP/baekjoon_2839.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3 
 # solution for https://www.acmicpc.net/problem/2839
 
+import sys
+sys.setrecursionlimit(1500)
+
 class Packing(object):
     def __init__(self):
         self.weights = [3, 5]
-
+    #memo{1:inf, 2:inf, 3:1, 4:inf, 5:1, 6:2, ...}
     def measure(self, w_to_measure):
         memo = {}
-        def measure_helper(n, remaining):
+        def measure_helper(remaining):
             if remaining in memo:
                 return memo[remaining]
-            elif remaining == 0:
-                return n
+            if remaining == 0:
+                return 0 
             elif remaining < 2:
                 return float('inf')
             else:
-                ans = min(measure_helper(n+1, remaining-self.weights[0]), measure_helper(n+1, remaining-self.weights[1])) 
-                if((remaining not in memo) or memo[remaining]>ans):
+                ans = min(measure_helper(remaining-self.weights[0])+1, measure_helper(remaining-self.weights[1])+1)
+                if remaining not in memo or memo[remaining] > ans:
                     memo[remaining] = ans
-                    print(memo)
                 return ans
-
-        measure_helper(0, w_to_measure)
+        measure_helper(w_to_measure)
+        print(memo)
         if memo[w_to_measure] == float('inf'):
             return -1
         else:
