8e8c847031f74353cc8774c1aac7f27eb1aec87a
suewoon <suewoon.ryu@gmail.com>
2017-09-13 02:43:09 +0900

    pass

--- DC/boardcover.py
+++ DC/boardcover.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 #  solution for https://algospot.com/judge/problem/read/BOARDCOVER
-
 import sys
 input = sys.stdin.readline
 
@@ -9,50 +8,66 @@ class Board(object):
         self.h = h
         self.w = w
         self.board = []
-        self.blocks = [[(0, 0), (1, 0), (0, 1)],[(0, 0), (0, 1), (1, 1)],
-                       [(0, 0), (1, 0), (1, 1)],[(0, 0), (1, 0), (1, -1)]]
-    
-    def make_board2(self, _string):
-        self.board.append(list(_string))
-    
-    def make_board(self):
-        for input_ in range(self.h):
-            self.board.append(list(input())[:-1])
-        print(self.board)
+        self.cache = {} 
+        self.blocks = [[(0, 0), (1, 0), (0, 1)], [(0, 0), (0, 1), (1, 1)],
+                       [(0, 0), (1, 0), (1, 1)], [(0, 0), (1, 0), (1, -1)]]
+
+    def read_line(self, string):
+        line = []
+        for char in string:
+            if char == '#':
+                line.append(1)
+            elif char == '.':
+                line.append(0)
+        self.board.append(line)
+
+    @staticmethod
+    def to_string(board):
+        return '\n'.join([''.join(['#' if e > 0 else '.' for e in row]) for row in board])
     
     def cover(self):
         board_to_cover = self.board[:]
         return self.cover_helper(board_to_cover)
-    
+
     def cover_helper(self, board):
+        def is_covered(board, x, y, block_type, revert):
+            covered = True
+            for i in range(3):
+                ny = y + self.blocks[block_type][i][0]
+                nx = x + self.blocks[block_type][i][1]
+                if (ny < 0 or ny >= len(board) or nx <0 or nx >= len(board[0])):
+                    covered  =  False
+                elif (board[ny][nx]+revert) > 1:
+                    covered =  False
+                if not (ny < 0 or ny >= len(board) or nx <0 or nx >= len(board[0])):
+                    board[ny][nx] += revert
+            return covered
+
         available = False
-        
         for i in range(self.h):
             for j in range(self.w):
-                if board[i][j] == '.':
+                if board[i][j] == 0:
                     blank = (i, j)
-                    available = True
+                    available = True  # find a blank space
                     break
-            else :
+            else:
                 continue
             break
-    
+
+        board_str = self.to_string(board)
+        if board_str in self.cache:
+            return self.cache[board_str]
+
         if not available:
             return 1
         elif available:
             ans = 0
-            for block_type in self.blocks:
-                x, y = blank[0], blank[1]
-                delta_1_x, delta_1_y = block_type[1][0], block_type[1][1]
-                delta_2_x, delta_2_y = block_type[2][0], block_type[2][1]
-                if x+delta_1_x >= 0 and y+delta_1_y >=0 and x+delta_2_x >=0 and y+delta_2_y >=0 and x+delta_1_x < self.h and x+delta_2_x < self.h and y+delta_1_y <self.w and y+delta_2_y <self.w and board[x+delta_1_x][y+delta_1_y]== board[x+delta_2_x][y+delta_2_y]=='.' :
-                    #  print(x,y,delta_1_x,delta_1_y,delta_2_x,delta_2_y)
-                    #  self.print_board(board)
-                    board[x][y] = board[x+delta_1_x][y+delta_1_y] = board[x+delta_2_x][y+delta_2_y] = '#'
+            for block_type in range(4):
+                if is_covered(board, blank[1], blank[0], block_type, 1):
+                    # print(x,y,delta_1_x,delta_1_y,delta_2_x,delta_2_y)
                     ans += self.cover_helper(board)
-                    #  self.print_board(board)
-                    board[x][y] = board[x+delta_1_x][y+delta_1_y] = board[x+delta_2_x][y+delta_2_y] = '.'
-    
+                is_covered(board, blank[1], blank[0], block_type, -1)
+                self.cache[self.to_string(board)] = ans
         return ans
 
 def main():
@@ -60,7 +75,8 @@ def main():
     for i in range(testcases):
         (h, w) = tuple([int(x) for x in input().split()])
         b = Board(h, w)
-        b.make_board()
+        for i in range(h):
+            b.read_line(input())
         print(b.cover())
 
 
--- DC/test_boardcover.py
+++ DC/test_boardcover.py
@@ -6,19 +6,19 @@ from boardcover import Board
 def test_make_board_1():
     board = Board(3,7)
     for string in '#.....#\n#.....#\n##...##'.split('\n'):
-        board.make_board2(string)
+        board.read_line(string)
     assert board.cover() == 0
 
 
 def test_make_board_2():
     board = Board(3,7)
     for string in '#.....#\n#.....#\n##..###'.split('\n'):
-        board.make_board2(string)
+        board.read_line(string)
     assert board.cover() == 2
 
 
 def test_make_board_3():
     board = Board(8,10)
     for string in '##########\n#........#\n#........#\n#........#\n#........#\n#........#\n#........#\n##########'.split('\n'):
-        board.make_board2(string)
+        board.read_line(string)
     assert board.cover() == 1514
