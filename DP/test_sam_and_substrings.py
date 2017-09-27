e99d767786600bda5d08b4e14c6dc7bca1142d18
suewoon <suewoon.ryu@gmail.com>
2017-09-27 18:38:33 +0900

    import the other module

--- DP/test_sam_and_substrings.py
+++ DP/test_sam_and_substrings.py
@@ -1,7 +1,12 @@
-from sam_and_substrings import make_substr2
+from sam_and_substrings2 import make_substr2
 
 def test_case1():
     assert make_substr2('16') == 23
 
 def test_case2():
     assert make_substr2('123') == 164
+
+def test_case4():
+    assert make_substr2('22') == 26
+def test_case3():
+    assert make_substr2('213676822290') == 546421488
