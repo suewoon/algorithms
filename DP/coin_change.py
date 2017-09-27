4df7fa10f65d579e0bc7b211490225c4f1ec5d8b
suewoon <suewoon.ryu@gmail.com>
2017-09-27 21:25:02 +0900

    using lru_cache

--- DP/coin_change.py
+++ DP/coin_change.py
@@ -1,20 +1,20 @@
 #/usr/bin/env python3 
 # solution for https://www.hackerrank.com/challenges/coin-change
 
-
+from functools import lru_cache
 def get_ways(n, c):
-    def get_ways_helper(n, sub_sets):
+    @lru_cache(maxsize=32)
+    def get_ways_helper(n, m):
         if n < 0:
             return 0
         if n == 0:
-            print("matched", sub_sets)
             return 1
-        ans = 0
-        for coin in c:
-            ans += get_ways_helper(n-coin, sub_sets+[coin])
-        return ans
 
-    return get_ways_helper(n, [])
+        if m<=0 and n>=1:
+            return 0
+        return get_ways_helper(n, m-1) + get_ways_helper(n-c[m-1], m)
+
+    return get_ways_helper(n, len(c))
 
 
 def main():
