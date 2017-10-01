34de1bd6c917a28acd73634c37330d57e7194b08
suewoon <suewoon.ryu@gmail.com>
2017-10-02 00:28:38 +0900

    passed

--- DP/coin_change.py
+++ DP/coin_change.py
@@ -3,8 +3,19 @@
 
 from functools import lru_cache
 def get_ways(n, c):
+
     @lru_cache(maxsize=32)
     def get_ways_helper(n, m):
+        table = [[ 0 for i in range(m)] for i in range(n+1)]
+
+        for i in range(m):
+            # base case: n==0 일때 return 1
+            table[0][i] = 1
+
+        for i in range(1, n+1):
+            for j in range(m):
+                x = table[i-c[i]][j] if i - c[j] >=0 else 0
+
         if n < 0:
             return 0
         if n == 0:
@@ -17,7 +28,7 @@ def get_ways(n, c):
         #c[m-1] 이 들어간 경우와 아예 안들어 간 경우 
         return get_ways_helper(n, m-1) + get_ways_helper(n-c[m-1], m)
 
-    return get_ways_helper
+    return get_ways_helper(n, len(c))
 
 
 def main():
