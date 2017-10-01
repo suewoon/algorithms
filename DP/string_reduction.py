e77107c06d2e13e0a9ff9032ea8230e8ab155999
suewoon <suewoon.ryu@gmail.com>
2017-10-02 00:27:57 +0900

    create a new file

---
+++ DP/string_reduction.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin python3
+# solution for https://www.hackerrank.com/challenges/string-reduction
+
+
+def stringReduction(a):
+    answer = 0
+    return answer
+# Tail starts here
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(0,t):
+        a=input()
+        print(stringReduction(a))
